4f3b1dc23f48269787aba01787e89ff52dd60bb0
EnggAmartech <Amartecheng@gmail.com>
2024-08-28 21:05:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,10 +38,10 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/services" element={<Projects />} />
           <Route path="/about" element={<About />} />
           {/* <Route path="/resume" element={<Resume />} /> */}
-          <Route path="/demoform" element={<Demoform />} />
+          <Route path="/contactus" element={<Demoform />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
